d8a6408f4bf02e98ecd4f9e2402071fb03ad278e
Kasyanov Maxim <maxkasser90@gmail.com>
2026-09-06 17:59:28 +0300

    Refactor solution for day 1 part 2

--- src/bin/day_1_part_2.rs
+++ src/bin/day_1_part_2.rs
@@ -1,69 +1,46 @@
-use std::collections::BTreeMap;
-
 use anyhow::Result;
 use aoc::{launch_solver, Solver};
+
+const DIGITS: [(&str, u32); 18] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+    ("1", 1),
+    ("2", 2),
+    ("3", 3),
+    ("4", 4),
+    ("5", 5),
+    ("6", 6),
+    ("7", 7),
+    ("8", 8),
+    ("9", 9),
+];
 
 struct Day1Part2Solver();
 
 impl Solver for Day1Part2Solver {
     fn solve(input_data: String) -> u32 {
-        let list_of_nums = BTreeMap::from([
-            ("one", 1),
-            ("two", 2),
-            ("three", 3),
-            ("four", 4),
-            ("five", 5),
-            ("six", 6),
-            ("seven", 7),
-            ("eight", 8),
-            ("nine", 9),
-            ("1", 1),
-            ("2", 2),
-            ("3", 3),
-            ("4", 4),
-            ("5", 5),
-            ("6", 6),
-            ("7", 7),
-            ("8", 8),
-            ("9", 9),
-        ]);
-        let data_lines = input_data.lines();
-        let mut sum = 0;
-        for data_line in data_lines {
-            let mut num_position = BTreeMap::<usize, &str>::new();
-            for key in list_of_nums.keys() {
-                match data_line.find(key) {
-                    Some(num_index) => {
-                        num_position.insert(num_index, &key);
-                    }
-                    None => {}
-                }
-                match data_line.rfind(key) {
-                    Some(num_index) => {
-                        num_position.insert(num_index, &key);
-                    }
-                    None => {}
-                }
-            }
-            let mut final_num = 0;
-            if let Some(first_num) = num_position.first_entry() {
-                final_num = final_num
-                    + list_of_nums
-                        .get(first_num.get())
-                        .expect("Can't find value")
-                        .clone()
-                        * 10;
-            }
-            if let Some(last_num) = num_position.last_entry() {
-                final_num = final_num
-                    + list_of_nums
-                        .get(last_num.get())
-                        .expect("Can't find value")
-                        .clone();
-            }
-            sum = sum + final_num;
-        }
-        sum
+        input_data
+            .lines()
+            .map(|line| {
+                let mut digits = (0..line.len()).flat_map(|i| {
+                    let rest = &line[i..];
+                    DIGITS.iter().filter_map(|(digit_as_str, digit)| {
+                        rest.starts_with(digit_as_str).then_some(digit)
+                    })
+                });
+
+                let first_digit = digits.next().unwrap_or(&0);
+
+                10 * first_digit + digits.next_back().unwrap_or(first_digit)
+            })
+            .sum::<u32>()
     }
 }
 fn main() -> Result<()> {
